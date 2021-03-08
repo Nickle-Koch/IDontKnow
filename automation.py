f92e4609ed4760a55597f4f66104e0be94cb34bc
Nickle-Koch <59538438+Nickle-Koch@users.noreply.github.com>
2021-03-09 00:09:41 +0200

    Add files via upload

---
+++ automation.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH)
+
+driver.get("https://youtube.com")
+elem = driver.find_element_by_name("search_query")
+
+elem2 = driver.find_element_by_id("dismissible")
+elem2.click()
+
